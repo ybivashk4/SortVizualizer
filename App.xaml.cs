7848762fd62be8e51d69379ee7b860f28770c8f6
mihail B <beltyukov-mihail@mail.ru>
2025-03-19 19:21:08 +0500

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SortVizualizer
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SortVizualizer
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
