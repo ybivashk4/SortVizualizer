d5e3d392d0899351f2be82aed1e5821a278159e8
mihail B <beltyukov-mihail@mail.ru>
2025-03-21 13:32:19 +0500

    Mikhail/Base Vm

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Diagnostics.Metrics;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
 
@@ -7,36 +8,118 @@ namespace SortVizualizer
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-            
+
         public MainPage()
         {
             InitializeComponent();
             BindingContext = new Vizualizer_ViewModel();
         }
 
-        public class Vizualizer_ViewModel : INotifyPropertyChanged
+        
+    }
+    public class Vizualizer_ViewModel : INotifyPropertyChanged
+    {
+        public ICommand PickSort { get; set; }
+        public delegate void SortFunc(ref ObservableCollection<Item> data);
+        public ICommand OnPickerSelectedIndexChanged { get; set; }
+
+        public static void BubleSort(ref ObservableCollection<Item> data)
         {
-
-            public ObservableCollection<int> Items { get; set; }
-
-            public Vizualizer_ViewModel()
+            for (int i=0;i<100;i++)
             {
-                Items = new ObservableCollection<int>();
-                for (int i=1;i<101;i++)
+                for (int j=0;j<100;j++)
                 {
-                    Items.Add(i);
+                    if (data[i] > data[j])
+                    {
+                        (data[i], data[j]) = (data[j], data[i]);
+                    }
                 }
-
-
-            }
-
-            public event PropertyChangedEventHandler? PropertyChanged;
-            protected virtual void OnPropertyChanged(string propertyName)
-            {
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
             }
         }
-    }
 
+        public static void Sort(ref ObservableCollection<Item> data, SortFunc sortFunc)
+        {
+            sortFunc(ref data);
+        }
+
+        public ObservableCollection<Item> Items { get; set; }
+
+        public Vizualizer_ViewModel()
+        {
+            PickSort = new Command<object>(pickSort);
+            Items = new ObservableCollection<Item>();
+            OnPickerSelectedIndexChanged = new Command<int>(onPickerSelectedIndexChanged);
+            for (int i = 1; i < 101; i++)
+            {
+                Items.Add(new Item(i));
+            }
+
+
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void pickSort(object parametr)
+        {
+            pickSort(parametr, Items);
+        }
+
+        private void pickSort(object parametr, ObservableCollection<Item> items)
+        {
+            if (parametr is int SortId)
+            {
+
+                switch (SortId)
+                {
+                    case 1:
+                        Sort(ref items, BubleSort);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+
+        // Нейро-код
+        private void onPickerSelectedIndexChanged(int selectedIndex)
+        {
+            
+        }
+
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+    public class Item : BindableObject
+    {
+        private int _value;
+        
+        public Item(int i) { Value = i; }
+        public static bool operator >(Item item1, Item item2)
+        {
+            return item1.Value > item2.Value;
+        }
+
+        public static bool operator <(Item item1, Item item2)
+        {
+            return item1.Value < item2.Value;
+        }
+
+        public int Value 
+        { 
+            get => _value; 
+            set
+            {
+                if (_value != value)
+                {
+                    _value = value;
+                    OnPropertyChanged(); // Уведомляем об изменении
+                }
+            }
+        }
+
+    }
 }
